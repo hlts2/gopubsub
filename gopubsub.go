687123d0e2ad6ea704ace0d6ace7b6b76900a4e5
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-09 23:28:31 +0900

    added: initial source

--- gopubsub.go
+++ gopubsub.go
@@ -1,1 +1,19 @@
 package gopubsub
+
+// PubSub ...
+type PubSub struct {
+}
+
+// NewPubSub returns PubSub object
+func NewPubSub() *PubSub {
+	return nil
+}
+
+// Subscribe ...
+func (p *PubSub) Subscribe(topig string, fu func()) {
+}
+
+// Publish ...
+func (p *PubSub) Publish(topig string) {
+
+}
