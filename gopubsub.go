7b3f508120c27577f89a404b2a5f222ea0e013bb
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-10 14:22:11 +0900

    added: setting for pubsub of capacity

--- gopubsub.go
+++ gopubsub.go
@@ -1,19 +1,52 @@
 package gopubsub
 
+import (
+	"sync"
+)
+
+const (
+	defaultPubSubTopicCapacity = 100000
+	initialSubscriberCapacity  = 100
+)
+
 // PubSub ...
 type PubSub struct {
+	mu         *sync.Mutex
+	registries []registry
+}
+
+type registry struct {
+	topic       string
+	subscribers []chan interface{}
 }
 
 // NewPubSub returns PubSub object
 func NewPubSub() *PubSub {
-	return nil
+	ps := &PubSub{
+		mu:         new(sync.Mutex),
+		registries: make([]registry, defaultPubSubTopicCapacity),
+	}
+
+	for i := 0; i < defaultPubSubTopicCapacity; i++ {
+		ps.registries[i].subscribers = make([]chan interface{}, initialSubscriberCapacity)
+	}
+
+	go ps.start()
+	return ps
+}
+
+func (p *PubSub) start() {
 }
 
 // Subscribe ...
-func (p *PubSub) Subscribe(topig string, fu func()) {
+func (p *PubSub) Subscribe(topic string) <-chan interface{} {
+	ch := make(chan interface{}, 1)
+
+	// TODO generate hash
+
+	return ch
 }
 
 // Publish ...
-func (p *PubSub) Publish(topig string) {
-
+func (p *PubSub) Publish(topic string, message interface{}) {
 }
--- gopubsub_test.go
+++ gopubsub_test.go
@@ -1,1 +1,18 @@
 package gopubsub
+
+import (
+	"testing"
+)
+
+func TestNewPubSub(t *testing.T) {
+	ps := NewPubSub()
+
+	if ps == nil {
+		t.Errorf("NewPubSub is nil")
+	}
+
+	got := len(ps.registries)
+	if defaultPubSubTopicCapacity != got {
+		t.Errorf("PubSub capacity is wrong. expected: %v, got: %v", defaultPubSubTopicCapacity, got)
+	}
+}
